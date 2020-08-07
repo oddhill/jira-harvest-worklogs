6fc55eb78feca4ecd6c8eeb6a7f5142c39c114ab
Olof Bokedal <olof.bokedal@oddhill.se>
2020-08-07 14:57:45 +0200

    Provide feedback.

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,11 +21,47 @@ $(document).ready(function() {
   const loadingText = $submitButton.data('loading-text');
   const waitingText = $submitButton.data('waiting-text');
   const $projectSelect = $form.find('#project');
+  let baseUrl = '';
+
+  AP.getLocation(function(location){
+    const pathArray = location.split( '/' );
+    console.log(pathArray);
+    baseUrl = pathArray[0] + '//' + pathArray[2];
+  });
 
   $projectSelect.chosen({
     inherit_select_classes: true,
     include_group_label_in_selected: true
   });
+
+  const updateResults = function(result) {
+    let html = '';
+
+    if ($.isEmptyObject(result)) {
+      html = '<h2>No issues were affected</h2>';
+    }
+    else {
+      html = '<h2>The following issues were affected</h2>';
+      html += '<ul>';
+      $.each(result, function(issueKey, seconds) {
+        if (seconds != 0) {
+          const hours = Math.abs(seconds / 3600);
+
+          html += '<li>';
+          html += '<a href="' + baseUrl + '/browse/' + issueKey + '">' + issueKey + '</a>: ';
+          if (seconds > 0) {
+            html += 'Added ' + hours + ' hours';
+          } else {
+            html += 'Removed ' + hours + ' hours';
+          }
+          html += '</li>';
+        }
+      });
+      html += '</ul>';
+    }
+
+    $('#results').html(html);
+  };
 
   $.ajax({
     url: '/projects',
@@ -52,11 +88,16 @@ $(document).ready(function() {
     },
     success: function(responseText, statusText) {
       $submitButton.html(waitingText).prop('disabled', false);
+      const affectedIssues = {};
+      updateResults(affectedIssues);
 
       $.each(responseText, function(issueKey, entries) {
         const filterFrom = Date.parse($form.find('[name=from]').val() + ' 00:00:00+00');
         const filterTo = Date.parse($form.find('[name=to]').val() + ' 23:59:59+00');
         let existingWorkLogs = {};
+        if (!affectedIssues.hasOwnProperty(issueKey)) {
+          affectedIssues[issueKey] = 0;
+        }
 
         AP.require('request', function(request) {
           request({
@@ -90,7 +131,10 @@ $(document).ready(function() {
                       data: JSON.stringify(data),
                       contentType: 'application/json',
                       success: function (response) {
-                        console.log(response);
+                        response = JSON.parse(response);
+                        existingWorkLogs[entry.id] = response;
+                        affectedIssues[issueKey] += response.timeSpentSeconds;
+                        updateResults(affectedIssues);
                       },
                       error: function () {
                         console.log(arguments);
@@ -106,7 +150,11 @@ $(document).ready(function() {
                       data: JSON.stringify(data),
                       contentType: 'application/json',
                       success: function (response) {
-                        console.log(response);
+                        let diff = JSON.parse(response).timeSpentSeconds - existingWorkLogs[entry.id].timeSpentSeconds;
+                        if (diff != 0) {
+                          affectedIssues[issueKey] += diff;
+                          updateResults(affectedIssues);
+                        }
                       },
                       error: function () {
                         console.log(arguments);
@@ -132,7 +180,8 @@ $(document).ready(function() {
                         url: '/rest/api/latest/issue/' + issueKey + '/worklog/' + worklog.id,
                         type: 'DELETE',
                         success: function (response) {
-                          console.log(response);
+                          affectedIssues[issueKey] -= worklog.timeSpentSeconds;
+                          updateResults(affectedIssues);
                         },
                         error: function () {
                           console.log(arguments);

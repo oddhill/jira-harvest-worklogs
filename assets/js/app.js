e0a0035660f10f9459bb751c659cccca72e659c3
Olof Bokedal <olof.bokedal@oddhill.se>
2020-08-07 09:44:50 +0200

    Create new entries and update existing ones.

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,26 +54,71 @@ $(document).ready(function() {
       $submitButton.html(waitingText).prop('disabled', false);
 
       $.each(responseText, function(issueKey, entries) {
-        $.each(entries, function(index, entry) {
-          const data = {
-            started: entry.date,
-            timeSpentSeconds: entry.seconds,
-            comment: entry.notes
-          };
+        const filterFrom = Date.parse($form.find('[name=from]').val() + ' 00:00:00+00');
+        const filterTo = Date.parse($form.find('[name=to]').val() + ' 23:59:59+00');
+        let existingWorkLogs = {};
 
-          AP.require('request', function(request) {
-            request({
-              url: '/rest/api/latest/issue/' + issueKey + '/worklog',
-              type: 'POST',
-              data: JSON.stringify(data),
-              contentType: 'application/json',
-              success: function(response) {
-                console.log(response);
-              },
-              error: function() {
-                console.log(arguments);
-              }
-            });
+        AP.require('request', function(request) {
+          request({
+            url: '/rest/api/latest/issue/' + issueKey  + '/worklog',
+            type: 'GET',
+            contentType: 'application/json',
+            success: function(response) {
+              response = JSON.parse(response);
+
+              $.each(response.worklogs, function(index, worklog) {
+                const entryDate = Date.parse(worklog.started.substring(0, worklog.started.indexOf('T')));
+
+                if (entryDate >= filterFrom && entryDate <= filterTo) {
+                  const harvestID = worklog.comment.content[1].content[2].text.replace('Harvest ID: ', '');
+                  existingWorkLogs[harvestID] = worklog;
+                }
+              });
+
+              $.each(entries, function(index, entry) {
+                const data = {
+                  started: entry.date,
+                  timeSpentSeconds: entry.seconds,
+                  comment: entry.notes + "\n\nUser: " + entry.user + "\nHarvest ID: " + entry.id
+                };
+
+                if (!existingWorkLogs.hasOwnProperty(entry.id)) {
+                  AP.require('request', function (request) {
+                    request({
+                      url: '/rest/api/latest/issue/' + issueKey + '/worklog',
+                      type: 'POST',
+                      data: JSON.stringify(data),
+                      contentType: 'application/json',
+                      success: function (response) {
+                        console.log(response);
+                      },
+                      error: function () {
+                        console.log(arguments);
+                      }
+                    });
+                  });
+                }
+                else {
+                  AP.require('request', function (request) {
+                    request({
+                      url: '/rest/api/latest/issue/' + issueKey + '/worklog/' + existingWorkLogs[entry.id].id,
+                      type: 'PUT',
+                      data: JSON.stringify(data),
+                      contentType: 'application/json',
+                      success: function (response) {
+                        console.log(response);
+                      },
+                      error: function () {
+                        console.log(arguments);
+                      }
+                    });
+                  });
+                }
+              });
+            },
+            error: function() {
+              console.log(arguments);
+            }
           });
         });
       });
